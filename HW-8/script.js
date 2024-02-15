1e05fc15ad54eabc99c0eca81069d7d846560923
Mira <154324598+Miraamal@users.noreply.github.com>
2024-02-15 19:12:19 +0100

    Add files via upload

---
+++ HW-8/script.js
@@ -0,0 +1,18 @@
+/* 1.const numbersDiv = document.querySelector('.numbers');
+for (let i = 100; i >= 50; i -= 10) {
+    const paragraph = document.createElement('p');
+    paragraph.textContent = 'Number:' + i;
+    numbersDiv.appendChild(paragraph);
+  }
+
+  2.
+ const stringsArray = [ "random string 1", "random string 2", "random string 3"]
+ const stringsContainer = document.querySelector('.strings_container');
+ for (let i = 0; i < stringsArray.length; i++) {
+    const paragraph = document.createElement('p');
+    paragraph.textContent = stringsArray[i];
+    stringsContainer.appendChild(paragraph);
+  }
+*/
+
+
